514b8d3852d6e37752f0b510d4e794514f8f537d
micahweihert <micah.weihert@victoruno.com>
2023-08-11 18:50:51 -0400

    Add files via upload

---
+++ RobotChargingLearned/-data-updates.lua
@@ -0,0 +1,6 @@
+-- data.raw.roboport.roboport.charging_energy = "1000kW" 
+data.raw.roboport.roboport.charging_energy = "1210MW" 
+
+
+-- only two charging points:
+--data.raw.roboport.roboport.charging_offsets = {{1.5,1.5},{-1.5,1.5}}
